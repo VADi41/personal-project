a0454186997d4ff8a0b74fef351d70e518d84a25
VADi41 <38165318+VADi41@users.noreply.github.com>
2018-05-12 18:46:36 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
-$('a[href^="#"]').on('click', function(event) {
+$('.nav-item[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+$('.navbar-brand[href^="#"]').on('click', function(event) {
 
     var target = $(this.getAttribute('href'));
 
