8008d65a32f1d2e02dc2f78271b56517e8d2ff96
VADi41 <38165318+VADi41@users.noreply.github.com>
2018-05-12 21:43:34 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,48 @@
 $('.nav-item[href^="#"]').on('click', function(event) {
-
     var target = $(this.getAttribute('href'));
-
     if( target.length ) {
         event.preventDefault();
         $('html, body').stop().animate({
             scrollTop: target.offset().top
         }, 1000);
     }
-
 });
+
 $('.navbar-brand[href^="#"]').on('click', function(event) {
-
     var target = $(this.getAttribute('href'));
-
     if( target.length ) {
         event.preventDefault();
         $('html, body').stop().animate({
             scrollTop: target.offset().top
         }, 1000);
     }
+});
+
+/*$(window).on('scroll', function(){
+  if($(window).scrollTop()){
+    $('nav').addClass('fadeIn');
+    $('nav-item').addClass('fadeIn');
+  }
+  else{
+    $('nav').removeClass('fadeIn');
+    $('nav-item').addClass('fadeIn');
+  }
+})*/
+
+(function($) {
+    var ctrl = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: 'onLeave'
+        }
+    });
+})(jQuery);
+
+$("section").each(function() {
+
+   new ScrollMagic.Scene({
+       triggerElement: this
+   })
+   .setPin(this)
+   .addTo(ctrl);
 
 });
